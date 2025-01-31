970450e05e9cad4f0f3a95126d2f800d42a48a7b
gowrishan2 <gowrishangararl@gmail.com>
2025-01-31 10:22:32 +0530

    adding features

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,15 +103,17 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
+    await sql.begin(async (tx) => {
+      await seedUsers();
+      await seedCustomers();
+      await seedInvoices();
+      await seedRevenue();
+    });
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
+    console.error('Seeding error:', error);
     return Response.json({ error }, { status: 500 });
   }
 }
+
